e91a3a69dcbb90045f2448e6710a34268427667c
siino-xyz <b114807h@gmail.com>
2022-03-23 19:41:10 +0900

    [fix]microCMSのAPIキー名を変更 npm run build確認済み

--- libs/client.js
+++ libs/client.js
@@ -2,5 +2,5 @@ import { createClient } from "microcms-js-sdk";
 
 export const client = createClient({
   serviceDomain: 'kp822wg687',
-  apiKey: process.env.API_KEY
+  apiKey: process.env.MICROCMS_API_KEY
 })
--- pages/index.jsx
+++ pages/index.jsx
@@ -58,7 +58,7 @@ export default function Home({articles}) {
 
 export const getStaticProps = async () => {
   const key = {
-    headers: {'X-MICROCMS-API-KEY': process.env.API_KEY},
+    headers: {'X-MICROCMS-API-KEY': process.env.MICROCMS_API_KEY},
   };
   const data = await fetch('https://kp822wg687.microcms.io/api/v1/articles?offset=0&limit=3', key)
     .then(res => res.json())
