6cf5747872e29e8f5494791ca019930320902168
siino-xyz <b114807h@gmail.com>
2022-04-07 21:04:06 +0900

    fonta

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -13,9 +13,9 @@ export default class MyDocument extends Document {
           <link rel="shortcut icon" href="/static/favicon.ico" />
 
           {/* google fonts  */}
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com"  />
-          <link href="https://fonts.googleapis.com/css2?family=BIZ+UDPGothic:wght@400;700&family=MuseoModerno:wght@500;600&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=BIZ+UDPGothic:wght@400;700&family=MuseoModerno:wght@500;600&display=swap" rel="stylesheet" /> */}
 
 
           {/* Inject MUI styles first to match with the prepend: true configuration. */}
