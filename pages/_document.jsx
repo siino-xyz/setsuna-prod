f830035a8e1f757e682d057cae80a16be6e2164b
siino-xyz <b114807h@gmail.com>
2022-04-07 21:20:39 +0900

    asdf

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -13,16 +13,16 @@ export default class MyDocument extends Document {
           <link rel="shortcut icon" href="/static/favicon.ico" />
 
           {/* google fonts  */}
-          {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com"  />
-          <link href="https://fonts.googleapis.com/css2?family=BIZ+UDPGothic:wght@400;700&family=MuseoModerno:wght@500;600&display=swap" rel="stylesheet" /> */}
-
-
-
-
           <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com"  />
+          <link href="https://fonts.googleapis.com/css2?family=BIZ+UDPGothic:wght@400&family=MuseoModerno:wght@700&display=swap" rel="stylesheet" />
+
+
+
+
+          {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=BIZ+UDPGothic&display=swap" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css2?family=BIZ+UDPGothic&display=swap" rel="stylesheet" /> */}
 
 
 
